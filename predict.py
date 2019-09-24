e424da59f3aa528929383c2cda0f53ebe6bf645d
Wladimir Gramacho <wladimirgramacho@gmail.com>
2019-09-24 10:14:22 -0300

    feat: feature scaling

--- predict.py
+++ predict.py
@@ -8,10 +8,13 @@ from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score, recall_score
+from sklearn.preprocessing import StandardScaler
 
 df1 = pd.read_pickle('first_two_semesters_failed_courses.pkl')
 df2 = pd.read_pickle('first_two_semesters_grades.pkl')
 df3 = pd.read_pickle('first_two_semesters_grades_workload.pkl')
+
+scaler = StandardScaler()
 
 logreg_param_grid = {
   'solver': ['liblinear', 'lbfgs'],
@@ -20,11 +23,7 @@ logreg_param_grid = {
 }
 
 mlpc_param_grid = {
-  'hidden_layer_sizes': [(50,50,50), (50,100,50), (100,)],
-  'activation': ['tanh', 'relu'],
-  'solver': ['sgd', 'adam'],
-  'alpha': [0.0001, 0.05],
-  'learning_rate': ['constant','adaptive'],
+  'alpha': 10.0 ** -np.arange(1, 7)
 }
 
 dtree_param_grid = {
@@ -41,7 +40,7 @@ rf_param_grid = {
 
 classifiers = [
   ('LogisticRegression', LogisticRegression(max_iter=300), logreg_param_grid),
-  # ('MLPClassifier', MLPClassifier(max_iter=100), mlpc_param_grid),
+  ('MLPClassifier', MLPClassifier(max_iter=100), mlpc_param_grid),
   ('DecisionTreeClassifier', tree.DecisionTreeClassifier(), dtree_param_grid),
   ('RandomForestClassifier', RandomForestClassifier(), rf_param_grid)
 ]
@@ -54,6 +53,10 @@ for index, df in enumerate([df1, df2, df3]):
   X_train, X_test, y_train, y_test = train_test_split(
       features, labels, test_size=test_size, stratify=labels, random_state=42)
 
+  scaler.fit(X_train.values)
+  X_train = scaler.transform(X_train.values)
+  X_test = scaler.transform(X_test.values)
+
   print('Model', index+1)
   print('Dataset - number of columns:', feature_cols.size)
   print('Dataset - number of rows:', len(features))
@@ -61,17 +64,16 @@ for index, df in enumerate([df1, df2, df3]):
   print('Test size:', len(X_test))
   print('\n')
 
-
   for classifier in classifiers:
     print(classifier[0])
     grid_search = GridSearchCV(classifier[1], classifier[2], scoring='recall',
                             cv=10, return_train_score=True)
-    grid_search.fit(X_train.values, y_train.values)
+    grid_search.fit(X_train, y_train.values)
 
-    y_pred = grid_search.predict(X_test.values)
+    y_pred = grid_search.predict(X_test)
     print('Best params for recall', grid_search.best_params_)
-    print("recall = %0.4f" % recall_score(y_test, y_pred))
-    print("accuracy = %0.4f" % accuracy_score(y_test, y_pred))
+    print("recall = %0.4f" % recall_score(y_test.values, y_pred))
+    print("accuracy = %0.4f" % accuracy_score(y_test.values, y_pred))
     # file_name = 'tree' + str(index+1) + '.dot'
     # tree.export_graphviz(grid_search.best_estimator_, max_depth=1, out_file=file_name, feature_names=list(feature_cols), class_names=['FORMADO', 'EVADIDO'], filled=True, label='root', impurity=False)
 
